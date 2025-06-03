cc93cc2a4c56dab9f792f4c78716ff206ac7cbfa
Ros-spec <rjge022604@gmail.com>
2025-06-03 17:31:07 -0500

    Update con.php

--- con.php
+++ con.php
@@ -1,8 +1,14 @@
 <?php
-$host = "sql202.infinityfree.com";
-$dbname = "if0_39018236_prueba";
-$user = "if0_39018236";
-$pass = "9KXouMvOYC";
+// $host = "sql202.infinityfree.com";
+// $dbname = "if0_39018236_prueba";
+// $user = "if0_39018236";
+// $pass = "9KXouMvOYC";
+
+$host = 'caboose.proxy.rlwy.net';
+$port = '44486';
+$dbname = 'railway';
+$user = 'root';
+$password = 'LtnSDbWbdaJCyOhqZreXJSMbKggVVTod';
 
 $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass, [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
