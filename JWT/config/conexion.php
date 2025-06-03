a437c3514b5b346ed6ef72060e6162cc57ecbc43
Ros-spec <rjge022604@gmail.com>
2025-06-03 05:32:49 -0500

    Update conexion.php

--- JWT/config/conexion.php
+++ JWT/config/conexion.php
@@ -1,4 +1,8 @@
 <?php
+$pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $user, $pass, [
+    PDO::ATTR_TIMEOUT => 3,
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+]);
 
 class Conexion {
     private $con;
