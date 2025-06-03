e307f3ec8dc5be8c9cc8a56d1ccedf4e781ae060
Ros-spec <rjge022604@gmail.com>
2025-06-03 05:36:36 -0500

    Update test.php

--- test.php
+++ test.php
@@ -1,7 +1,13 @@
-<?php
+<?php require_once '/JWT/config/conexion.php';
+
+    $host = "sql202.infinityfree.com";
+    $bd   = "if0_39018236_prueba";
+    $user = "if0_39018236";
+    $pw   = "9KXouMvOYC";
+
 header("Content-Type: application/json");
 try {
-    $pdo = new PDO("mysql:host=REMOTO_HOST;dbname=DB", "USUARIO", "CLAVE", [
+    $pdo = new PDO("mysql:host=$host ;dbname=$bd", $user , $pw, [
         PDO::ATTR_TIMEOUT => 3,
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
     ]);
