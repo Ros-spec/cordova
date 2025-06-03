86405149de41a02ddd09286256460bf7e39d8921
Ros-spec <rjge022604@gmail.com>
2025-06-03 05:44:24 -0500

    Update test.php

--- test.php
+++ test.php
@@ -1,17 +1,6 @@
-<?php require_once 'JWT/config/conexion.php';
-
-    $host = "sql202.infinityfree.com";
-    $bd   = "if0_39018236_prueba";
-    $user = "if0_39018236";
-    $pw   = "9KXouMvOYC";
+<?php require_once 'con.php';
 
 header("Content-Type: application/json");
-try {
-    $pdo = new PDO("mysql:host=$host;dbname=$bd", $user, $pw,[
-        PDO::ATTR_TIMEOUT => 3,
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-    ]);
-    echo json_encode(["status" => "ok"]);
-} catch (Exception $e) {
-    echo json_encode(["status" => "error", "msg" => $e->getMessage()]);
-}
+echo json_encode(["status" => "ok"]);
+
+?>
